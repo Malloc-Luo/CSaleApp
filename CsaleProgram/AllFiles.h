7e6e32537e08acaff609c9a3adb439216a89abbf
Malloc_Luo <2423049596@qq.com>
2019-12-06 11:54:00 +0800

    添加模块

---
+++ CsaleProgram/AllFiles.h
@@ -0,0 +1,50 @@
+/***************************************************************
+ * @Filename: AllFiles.h
+ * @Author: Malloc-Luo
+ * @Date: 2019-12-2
+ * @Description: 头文件汇总，各个文件仅需引用此文件作为头文件
+ *
+ * @Modified:
+ ***************************************************************/
+
+#ifndef __ALL_FILES_H__
+#define __ALL_FILES_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <thread>
+#include <string.h>
+
+#include "BeginPage.h"
+#include "Login.h"
+
+#define endline   printf("\n\n\n")
+#define newline   printf("\n")
+#define middle    printf("                      ")
+#define beginline {newline; middle; middle; }
+
+//error type
+#define NOCOMMAND          0x01
+#define USERNAME_DISABLE   0x02
+#define USERNAME_EXIST     0x03
+#define PASSWORD_DISABLE   0x04
+#define PASSWORD_DIFFERENT 0x05
+#define PASSWORD_ERROR     0x06
+#define USERNAME_NOT_EXIST 0x07
+
+#define CONFIRM 1
+#define CANCEL  2
+
+#define _EXIT 242
+
+void get_string(char *data);
+
+char get_char(void);
+
+void command_char(void);
+
+void error_remind(int );
+
+
+
+#endif // __ALL_FILES_H__
---
+++ CsaleProgram/AllMode.c
@@ -0,0 +1,67 @@
+#include "AllFiles.h"
+
+/*
+ * 获取字符串并清除缓冲区
+ */
+
+void get_string(char *data)
+{
+    gets(data);
+    fflush(stdin);
+}
+
+/*
+ * 读取一个字符并清空缓冲区
+ */
+char get_char(void)
+{
+    char ch;
+    ch = getchar();
+    fflush(stdin);
+
+    return ch;
+}
+
+/*
+ * 打印命令提示符
+ */
+void command_char(void)
+{
+    printf("$->  ");
+}
+
+/*
+ * 错误提示
+ */
+void error_remind(int error_type)
+{
+    switch(error_type)
+    {
+        case NOCOMMAND:
+            printf("命令无效，请重新选择");
+            break;
+        case USERNAME_DISABLE:
+            printf("用户名不可用，请按要求填写");
+            break;
+        case USERNAME_EXIST:
+            printf("用户名不可以，该用户名已被使用，请重新填写");
+            break;
+        case USERNAME_NOT_EXIST:
+            printf("用户不存在，请重新输入");
+            break;
+        case PASSWORD_DISABLE:
+            printf("密码不可用，请仔细阅读规范");
+            break;
+        case PASSWORD_DIFFERENT:
+            printf("密码验证不符合，请重新输入");
+            break;
+        case PASSWORD_ERROR:
+            printf("密码错误");
+            break;
+        default:
+            break;
+    }
+}
+
+
+
