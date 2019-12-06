ddc5a373ddb6e95d63a84421db3d934384c74607
Malloc_Luo <2423049596@qq.com>
2019-12-06 11:54:16 +0800

    添加开始模块

---
+++ CsaleProgram/BeginPage.c
@@ -0,0 +1,116 @@
+/************************************************************
+ * @filename: BeginPage.c
+ * @author: Malloc-Luo
+ * @date: 2019.12.4
+ * @Description: 开始页模块，用于绘制开始页，接收指令
+ *
+ ***********************************************************/
+#include "BeginPage.h"
+
+int Begin_page(void)
+{
+    int cmd = 0;
+
+    while(1)
+    {
+        cmd = Begin_page_windows();
+        cmd = Begin_page_callback(cmd);
+        if(cmd == _EXIT)
+            break;
+    }
+
+    return 0;
+}
+
+
+int OnlineFlag = LEAVE;
+/*
+ * 绘制开始界面，读取指令
+ */
+int Begin_page_windows()
+{
+    int cmd = 0;
+    int flag = 0;
+
+    system("cls");
+
+    //show windows simpily
+    endline; endline; newline;
+    middle; printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~开始~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"); endline;
+    if(OnlineFlag == LEAVE)
+    {
+        beginline; printf("$-> 登录         ->[1]"); newline;
+        beginline; printf("$-> 新用户注册   ->[2]"); newline;
+    }
+    beginline; printf("$-> 帮助         ->[3]"); newline;
+
+    if(OnlineFlag == LEAVE)
+    {
+        beginline; printf("$-> 退出         ->[4]"); newline;
+    }
+
+    if(OnlineFlag == ONLINE)
+    {
+        beginline; printf("$-> 注销登录        ->[5]"); newline;
+    }
+
+    do
+    {
+        beginline; command_char();
+        scanf("%d", &cmd);
+
+        if(OnlineFlag == LEAVE)
+        {
+            if(cmd != 1 && cmd != 2 && cmd != 3 && cmd != 4)
+                flag = 1;
+            else
+                flag = 0;
+        }
+
+        if(OnlineFlag == ONLINE)
+        {
+            if(cmd != 3 && cmd != 5)
+                flag = 1;
+            else
+                flag = 0;
+        }
+
+        if(flag)
+        {
+            beginline; error_remind(NOCOMMAND); newline;
+        }
+
+    }while(flag);
+
+    return cmd;
+}
+
+
+
+int Begin_page_callback(int cmd)
+{
+    int Command = 0;
+    switch(cmd)
+    {
+        case LOGIN:
+            break;
+        case SIGNIN:
+            break;
+       /*help complate*/
+        case HELP:
+            Help();
+            break;
+       /*exit complate*/
+        case EXIT:
+            Command = Exit();
+            break;
+        case SIGNOUT:
+            break;
+        default:
+            break;
+    }
+
+    return Command;
+
+
+}
---
+++ CsaleProgram/BeginPage.h
@@ -0,0 +1,38 @@
+/*******************************************************
+ * @Filename: BeginPage.h
+ * @Author: Malloc-Luo
+ * @Data: 2019-12-2
+ * @Description: 开始页，包括开始页函数
+ *******************************************************/
+
+#ifndef __BEGIN_PAGE_H__
+#define __BEGIN_PAGE_H__
+#include "AllFiles.h"
+
+//在线状态
+extern int OnlineFlag;
+
+#define ONLINE 0x00
+#define LEAVE  0x404
+
+int Begin_page(void);
+
+/*
+ * 可能会执行：
+ *          新用户注册
+ *          用户登录
+ *          帮助
+ *          退出
+ * 以下对应宏定义
+ */
+int Begin_page_windows(void);
+
+#define LOGIN       0x02 //注册
+#define SIGNIN      0x01 //登录
+#define HELP        0x03 //帮助
+#define EXIT        0x04 //退出
+#define SIGNOUT     0x05 //注销登录
+
+int Begin_page_callback(int flag);
+
+#endif // __BEGIN_PAGE_H__
