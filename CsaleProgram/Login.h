5da66af0c6d439098b33f22433ed5b3bfc7ce497
Malloc_Luo <2423049596@qq.com>
2019-12-06 11:55:38 +0800

    接下来是注册

---
+++ CsaleProgram/Login.h
@@ -0,0 +1,21 @@
+/************************************************************
+ * @filename: Login.h
+ * @author: Malloc-Luo
+ * @date: 2019.12.6
+ * @Description: ×¢²áÄ£¿é
+ *
+ ***********************************************************/
+#ifndef __LOGIN_H__
+#define __LOGIN_H__
+#include "AllFiles.h"
+
+int Login_page();
+
+int Login_windows(int cmd);
+
+int Login_system(int cmd);
+
+int Login_callback(int cmd);
+
+
+#endif // __LOGIN_H__
--- CsaleProgram/main.c
+++ CsaleProgram/main.c
@@ -1,8 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*************************************************************
+ * @Filename: main.c
+ * @Author: Malloc-Luo
+ * @Data: 2019-12-2
+ * @Description: 包含主函数部分，程序入口
+ *
+ * @Modified: 2019.12.06
+ *              do a test;
+ *
+ ************************************************************/
+
+#include "AllFiles.h"
 
 int main()
 {
-    printf("Hello world!\n");
+//    char ch;
+//    ch = get_char();
+//    printf("%c", ch);
+//   system("help.png");
+//   system("abc.html");
+  //Begin_page_windows();
+  Begin_page();
+  //Help();
     return 0;
 }
